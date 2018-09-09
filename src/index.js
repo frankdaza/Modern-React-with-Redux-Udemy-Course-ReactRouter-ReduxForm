2629801db170eefd557fe014e455763203facb1e
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-09 12:00:47 -0500

    The Basics of React Router.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class HelloWorld extends React.Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+class GoodBye extends React.Component {
+  render() {
+    return <div>Good Bye!</div>;
+  }
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>      
+      <Route path='/hi' component={ HelloWorld } />
+      <Route path='/bye' component={ GoodBye } />
+    </div>
+  </BrowserRouter>, 
+  document.getElementById('root'));
 registerServiceWorker();
