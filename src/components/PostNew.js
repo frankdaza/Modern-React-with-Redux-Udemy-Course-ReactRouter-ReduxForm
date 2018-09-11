3dc3fbdffd2b0145255f41b9148f0db6c043e717
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-10 20:33:36 -0500

    A react router gotcha.

---
+++ src/components/PostNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class PostNew extends Component {
+
+  render() {
+    return (
+      <div>
+        Post new!
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Promise from 'redux-promise';
 import { createStore, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 
 import PostIndex from './components/PostIndex';
+import PostNew from './components/PostNew';
 import reducers from './reducers/';
 
 
@@ -14,10 +15,11 @@ const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
 
 ReactDOM.render(
   <Provider store={ createStoreWithMiddleware(reducers) }>
-    <BrowserRouter>
-      <div>            
+    <BrowserRouter>      
+      <Switch>            
+        <Route path='/posts/new' component={ PostNew } />
         <Route path='/' component={ PostIndex } />
-      </div>
+      </Switch>
     </BrowserRouter>
   </Provider>, 
   document.getElementById('root'));
