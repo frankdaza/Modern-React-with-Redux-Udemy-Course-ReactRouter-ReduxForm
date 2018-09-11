121fb719145b15641bb78f413bd879c1bf54f63f
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-10 20:58:48 -0500

    Redux form.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import PostReducer from './PostReducer';
 
 const rootReducer = combineReducers({
-  posts: PostReducer
+  posts: PostReducer,
+  form: formReducer
 });
 
 export default rootReducer;
