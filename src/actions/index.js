ba2500da5d1d32c57a157c8c4e24c37c5d0edc8a
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-11 22:28:12 -0500

    Receiving New Posts.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import Axios from 'axios';
 
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POSTS = 'create_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
@@ -22,6 +23,15 @@ export function createPost(values, callback) {
 
   return {
     type: CREATE_POSTS,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = Axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
