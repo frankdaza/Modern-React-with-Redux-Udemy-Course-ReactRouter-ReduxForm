c8fd2c034bd74d4bc6a143d41f002a8def076cbb
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-11 22:44:34 -0500

    Selecting from OwnProps.

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 
-export default class PostShow extends Component {
+class PostShow extends Component {
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
 
   render() {
     return (
@@ -11,3 +18,9 @@ export default class PostShow extends Component {
     );
   };
 }
+
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostShow);
