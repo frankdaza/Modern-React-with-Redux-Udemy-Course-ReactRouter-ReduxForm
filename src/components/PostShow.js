d12b2624ab0f818ee6ff4aab3f2e9272a53d3b9f
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-11 22:53:28 -0500

    Data dependencies.

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -11,9 +11,17 @@ class PostShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Show post!
+        <h3>{ post.title }</h3>
+        <h6>Categories: { post.categories }</h6>
+        <p>{ post.content }</p>
       </div>
     );
   };
